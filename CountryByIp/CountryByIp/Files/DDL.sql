8e9abda3027b11f0c0192ac162cc69e996aa2870
vbodurov <vladimir@bodurov.com>
2014-08-28 12:05:48 -0700

    improved index

--- CountryByIp/CountryByIp/Files/DDL.sql
+++ CountryByIp/CountryByIp/Files/DDL.sql
@@ -30,10 +30,6 @@ CREATE NONCLUSTERED INDEX [IX_{0}_Country]
 	ON [dbo].[{0}]([Country])
 
 
-CREATE NONCLUSTERED INDEX [IX_{0}_FromIp] 
-	ON [dbo].[{0}]([FromIp])
-
-
-CREATE NONCLUSTERED INDEX [IX_{0}_ToIp] 
-	ON [dbo].[{0}]([ToIp] DESC) 
+CREATE NONCLUSTERED INDEX [IX_country_by_ip_FromIp_ToIp] 
+	ON [dbo].[yv_country_by_ip]([FromIp], [ToIp]) INCLUDE ([Country])
 
